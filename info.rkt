06d2a1222e109c787aa4759db4a0ef3e40d45157
Sara <sara@thoughtstem.com>
2020-02-12 09:33:41 -0800

    updating deps

--- info.rkt
+++ info.rkt
@@ -1,6 +1,7 @@
 #lang info
 (define collection "mc-coach")
-(define deps '("base"))
+(define deps '("base"
+               "https://github.com/thoughtstem/website.git"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/mc-coach.scrbl" ())))
 (define pkg-desc "Description Here")
